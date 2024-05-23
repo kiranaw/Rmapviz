62b4c9473d31fffebbd0dcc5e890db7332732b69
kiranaw <kirana.widyastuti@gmail.com>
2024-05-23 12:41:23 +0200

    update

--- ggplot_vs_leaflet.R
+++ ggplot_vs_leaflet.R
@@ -7,6 +7,8 @@ library(gganimate)
 map <- st_read("/home/kirana/Nextcloud/MO3viz/mapviz/maille.gpkg")
 breedingsiteupdated <- read_csv("/home/kirana/Nextcloud/MO3viz/processjson/simev_chunks/2603/csv/breedingsiteupdate.csv")
 
+proj4string(map)
+
 breedingsiteupdated <- breedingsiteupdated %>%
   mutate(day = (hour / 24) + 1) %>%
   select(day, wbs, wfbs, cell)
@@ -31,24 +33,15 @@ map_100 %>%
   theme(legend.position="bottom")
 
 
-######## generate series of map ###########
-library(lubridate)
-library(gganimate)
 
-sim_result <- st_as_sf(sim_result)
-
-p <- sim_result %>%
-  arrange(day) %>%
-  mutate(day=factor(day, levels=unique(day))) %>%
-  ggplot(aes(fill=wbs)) +
-  geom_sf(color="white", size=.1) +
-  theme_void() +
-  scale_fill_distiller(palette="YlOrRd", direction=1, guide=guide_legend(label.position="bottom", title.position="left", nrow=1), name="water in breeding site (ml)") +
-  theme(legend.position="bottom") +
-  labs(title="", subtitle="", caption="", tag="{current_frame}") +
-  #theme(legend.position=c(.15,-.09), legend.title=element_text(hjust=.5), plot.title=element_text(size=rel(1.5), family="sans", face="bold"), plot.subtitle=element_text(color="#5e5855"), plot.caption=element_text(color="#867e7a"), plot.tag=element_text(hjust=0.5, color="#5e5855"), plot.tag.position=c(0.5, 0.16), panel.background=element_rect(fill="#aad3df", color=NA)) +
-  transition_manual(day)
-
-animate(p, width=1080, height=755, renderer=file_renderer(dir="anim_mola_img/"))
-
-
+#with leaflet
+library(leaflet)
+leaflet(map_100) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+  addPolygons(
+    weight = 2,
+    opacity = 1,
+    color = "white",
+    fillOpacity = 0.7) 
