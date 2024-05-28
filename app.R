74e6819df4dbe5093e89c38788aef5b14b4eb058
kiranaw <kirana.widyastuti@gmail.com>
2024-05-28 13:04:34 +0200

    add shiny

---
+++ app.R
@@ -0,0 +1,46 @@
+library(shiny)
+
+createMap_for_a_day <- function(map_100){
+  
+  result_plot <- ggplot(map_100, aes(fill=wbs)) +
+    geom_sf(color="white", size=.1) +
+    theme_void() +
+    scale_fill_distiller(palette="YlOrRd", direction=1, guide=guide_legend(label.position="bottom", title.position="left", nrow=1), name="water in breeding site") +
+    theme(legend.position="bottom")
+  
+  
+  return(result_plot)
+}
+
+
+ui <- fluidPage(
+  title = "Day Map",
+  plotOutput("dailymap"),
+  sliderInput(
+    inputId = "day_in_slider",
+    label = "the day for the map",
+    min = 1, 
+    max = 365, 
+    value = 100,animate = T
+    
+  ),
+  textOutput("daytext")
+)
+
+server <- function(input, output) {
+  
+  daily_map_data <- reactive({
+    only_the_day <- dplyr::filter(breedingsiteupdated, day==input$day_in_slider)
+    daily_map_data <- inner_join(only_the_day, map,  by = c("cell" = "id_maille"))  %>% st_as_sf()
+  })
+  
+  output$dailymap <- renderPlot({
+    
+    createMap_for_a_day(daily_map_data())
+  })
+  
+  output$daytext <- reactive({input$day})
+  
+}
+
+shinyApp(ui = ui, server = server)
